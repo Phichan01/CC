280a6d47856f659fbf2f849bb386458fa6273b84
Phichan <sophiakatokobayashitsuchiya@gmail.com>
2023-08-17 15:30:54 -0300

    habibi 0101

---
+++ Estudo 15/07.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+    int x, y, z;
+    printf("entre com tres numeros inteiros:\n ");
+    scanf("%d %d %d", &x, &y, &z );
+    printf("soma = %d \n", x + y +z);
+    return 0;
+    }
