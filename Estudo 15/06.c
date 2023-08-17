a5a00d6bebdcbe80b1bdae67931366a95f573c2e
Phichan <sophiakatokobayashitsuchiya@gmail.com>
2023-08-17 14:09:29 -0300

    maodaods

---
+++ Estudo 15/06.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int d, dia, m, mes, ano, a;
+    scanf("%d", &dia);
+    a = dia/365;
+    ano = dia % 365;
+    mes = ano/30
+    dia = ano - (mes * 30);
+    printf ("%d ano(s)\n, %d mes(es)\n, dia(s)\n", a, mes, dia);
+    return 0;
+}
