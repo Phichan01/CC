8ecf7a7df7aff6f2b1b15c1f669a9524cef6a359
Phichan <sophiakatokobayashitsuchiya@gmail.com>
2023-08-17 15:31:25 -0300

    xzx\x\x

--- Estudo 15/06.c
+++ Estudo 15/06.c
@@ -4,7 +4,7 @@ int main (){
     scanf("%d", &dia);
     a = dia/365;
     ano = dia % 365;
-    mes = ano/30
+    mes = ano/30;
     dia = ano - (mes * 30);
     printf ("%d ano(s)\n, %d mes(es)\n, dia(s)\n", a, mes, dia);
     return 0;
