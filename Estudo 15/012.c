f8ff001fd22c9c2da5ac020d3570bfd8c9a91eec
Phichan <sophiakatokobayashitsuchiya@gmail.com>
2023-08-22 13:11:12 -0300

    dfsdfasf

--- Estudo 15/012.c
+++ Estudo 15/012.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
-int main () {
-    double salario, x;
-    int y;
-    scanf ("%lf", &salario);
-    x = salario;
-    if (salario >= 0 && salario <= 400){
-        salario += 0.15 * salario;
-        y = 15;
-    }   else
-    if (salario >= 400.01 && salario <= 800.00){
-        salario = salario + 0.12 * salario;
-        y = 12;
-    }   else
-    if (salario >= 800.01 && salario <= 1200.00){
-        salario += 0.1 * salario;
-        y = 10;
-    }   else
-    if (salario >= 1200.01 && salario <= 2000.00){
-        salario += 0.07 * salario;
-        y = 7; 
-    }   else 
-    if (salario > 2000.00){
-        salario += 0.04 * salario;
-        y = 4;
+
+int main() {
+    double salario, novoSalario, reajuste;
+    int indiceReajuste;
+
+    scanf("%lf", &salario);
+
+    if (salario >= 0 && salario <= 400.00) {
+        indiceReajuste = 15;
+        reajuste = 0.15 * salario;
+    } else if (salario <= 800.00) {
+        indiceReajuste = 12;
+        reajuste = 0.12 * salario;
+    } else if (salario <= 1200.00) {
+        indiceReajuste = 10;
+        reajuste = 0.10 * salario;
+    } else if (salario <= 2000.00) {
+        indiceReajuste = 7;
+        reajuste = 0.07 * salario;
+    } else {
+        indiceReajuste = 4;
+        reajuste = 0.04 * salario;
     }
-    printf ("novo salario: %.2lf\n", salario);
-    printf ("Reajuste ganho: %.2lf\n", salario - x);
-    printf ("Em percentual: %d %%\n", y);
+
+    novoSalario = salario + reajuste;
+
+    printf("Novo salario: %.2lf\n", novoSalario);
+    printf("Reajuste ganho: %.2lf\n", reajuste);
+    printf("Em percentual: %d %%\n", indiceReajuste);
+
     return 0;
-    }
+}
